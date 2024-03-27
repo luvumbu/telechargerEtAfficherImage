831139d7f0f3a685296ec93ea5a58e13473aad14
luvumbu <luvumbu.n@gmail.com>
2024-03-27 15:51:14 +0100

    ajout format musique ok

--- uploads/index.php
+++ uploads/index.php
@@ -1,5 +1,3 @@
-
-
 <div class="display">
     <?php
     $dir_nom = '.'; // dossier listé (pour lister le répertoir courant : $dir_nom = '.'  --> ('point')
@@ -17,28 +15,38 @@
     }
     if (!empty($fichier)) {
         sort($fichier); // pour le tri croissant, rsort() pour le tri décroissant
-
+    
         foreach ($fichier as $lien) {
-      
+
             $valeurf = "";
             $verif = false;
 
             $taille = strlen($lien);
             for ($i = 0; $i < $taille; $i++) {
-          
+
                 if ($lien[$i] == ".") {
                     $verif = true;
-                   
+
                 }
-                if ($verif == true &&  $lien[$i] != ".") {
+                if ($verif == true && $lien[$i] != ".") {
                     $valeurf = $valeurf . $lien[$i];
                 }
             }
- 
+
             if ($valeurf == "tiff" || $valeurf == "jpg" || $valeurf == "gif" || $valeurf == "png") {
-    ?>
+                ?>
                 <img src="<?php echo $lien ?>" alt="" srcset="">
-    <?php
+                <?php
+            }
+
+            if ($valeurf == "wav" || $valeurf == "flac" || $valeurf == "aac" || $valeurf == "ogg"  || $valeurf == "wma"|| $valeurf == "aiff"|| $valeurf == "alac"|| $valeurf == "dsd"|| $valeurf == "m4a"|| $valeurf == "mp3") {
+                ?>
+                <audio controls>
+                    <source src="<?php echo $lien ?>" type="audio/ogg">
+
+                    Your browser does not support the audio element.
+                </audio>
+                <?php
             }
         }
     }
